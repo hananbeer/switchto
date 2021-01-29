04feeab8f0bbc8443bb6c7949c5345a8f2593723
Hanan Beer <henymeny@gmail.com>
2021-01-29 04:53:29 +0200

    rename package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="switchto-hananbeer", # Replace with your own username
+    name="switchto", # Replace with your own username
     version="0.0.1",
     author="Hanan Beer",
     author_email="email@hanan.beer",
